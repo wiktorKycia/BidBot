096af318447785fd6abb97db8deb81a6c84703b5
wiktor <wiktor.kycia@gmail.com>
2026-05-18 14:18:20 +0200

    create the main structure of relevance-filter

--- etl/relevance-filter/main.py
+++ etl/relevance-filter/main.py
@@ -1,26 +1,46 @@
-
+import asyncio
+import os
 from pathlib import Path
-from etl.settings import MODEL, require_openai_api_key
+import logging
+
+from etl.utils import read_json
+from etl.settings import setup_logging, require_openai_api_key, MODEL, PARSED_DIR
 
 
 OPENAI_API_KEY = require_openai_api_key()
 
-DATA_PATH = Path(__file__).resolve().parent.parent.parent / "data"
-PARSED_JSON_PATH = DATA_PATH / "parsed"
 
-LOG_DIR = Path(__file__).resolve().parent / "logs"
-LOG_PATH = LOG_DIR / "vector_db.log"
+setup_logging()
+logger = logging.getLogger(__name__)
+
+async def tag_file(filename: str):
+    data: dict = await read_json(PARSED_DIR / filename)
+    if len(data['enrichment']['tags']) > 0:
+        return None
+    result = None
 
 
 
+    return result
 
-def main():
+
+async def main():
     pass
     '''
     read all the .json documents asynchronously
     check if they have tags
     if not perform a query to the llm that will create them
     '''
+    filenames = os.listdir(PARSED_DIR)
+
+    tasks = [tag_file(filename) for filename in filenames]
+
+    results = await asyncio.gather(*tasks, return_exceptions=True)
+
+    for res in results:
+        if isinstance(res, Exception):
+            logger.error(f"Błąd przy tagowaniu pliku: {res!r}")
+
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
